86b89aad0387637bd355ffddf6b7851331cf201b
shinaayomi <lateef718@gmail.com>
2021-06-23 14:20:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+// import Greet from './components/Greet'
+import { Greet } from './components/Greet'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
+      <Greet />
+      <Welcome />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,9 +21,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Shina
         </a>
-      </header>
+      </header> */}
+      
     </div>
   );
 }
---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1> View Shina </h1>
+// }
+export const Greet = () => <h1>Hello Ayomi</h1>
+
+// export default Greet
